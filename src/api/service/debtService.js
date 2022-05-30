37aa19906c2481302061547b9a190e031936bf5b
Krzysiek <krzychuuweb@gmail.com>
2022-05-30 19:51:57 +0200

    Change .gitignore and change route for getAllDebts function

--- src/api/service/debtService.js
+++ src/api/service/debtService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import {API_URL} from "../../utils/constants";
+import {getRoute} from "../../utils/routes";
 
 export async function getAllDebts() {
-    return await axios.get(API_URL + "/debts");
+    return await axios.get(API_URL + getRoute.debt.getAll);
 }
