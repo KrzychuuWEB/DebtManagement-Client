3ac6255273d0c4275bf2f5f2625df0ecedf19ea0
Krzysiek <krzychuuweb@gmail.com>
2022-05-31 21:52:56 +0200

    Change folder for theme.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import ApplicationThemeConfig from './components/template/theme';
+import ApplicationThemeConfig from './utils/theme';
 import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
