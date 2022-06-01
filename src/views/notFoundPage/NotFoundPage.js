5868a0afe04f31eaf6c0df2ade9d9ccb3e6c8c26
Krzysiek <krzychuuweb@gmail.com>
2022-06-01 18:54:39 +0200

    Change not found page design

--- src/views/notFoundPage/NotFoundPage.js
+++ src/views/notFoundPage/NotFoundPage.js
@@ -1,13 +1,13 @@
 import React from "react";
-import {Button} from "@mui/material";
-import {getRoute} from "../../utils/routes";
+import ExceptionTemplate from "../../components/exceptions";
 
 const NotFoundPage = () => {
     return (
-        <div>
-            Page not found!
-            <Button variant="outlined" href={getRoute.home}>Przejdz na stronę główną</Button>
-        </div>
+        <ExceptionTemplate
+            code={404}
+            description="Niestety nie znalezliśmy strony której szukasz :("
+            title="Not Found"
+        />
     );
 };
 
